8eb398793136b7d73732fe4ee8015dfca39ab4a3
MaTtiaa01 <mattia2001@live.it>
2022-10-21 15:53:13 +0200

    add eventlistener next

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,17 @@ titolo
 descrizione Creare un carosello come nella foto allegata.
 Milestone 0:
 Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
+
 Milestone 1:
 Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
+    -mi seleziono le immagini di ogni oggetto e le inserisco dinamicamente 
+
 Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+    -mi creo una classe active da mettere e togliere con .toggle alle varie immagini
+    -mi seleziono il testo e il titolo con map per poi inserirlo con ${}
+    -
+
+
 Milestone 2:
 Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
 BONUS 1 (opzionale):
@@ -20,24 +28,71 @@ Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
 
 const images = [
     {
-        image: 'img/01.webp',
+        image: '01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
-        image: 'img/02.webp',
+        image: '02.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
     }, {
-        image: 'img/03.webp',
+        image: '03.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     }, {
-        image: 'img/04.webp',
+        image: '04.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
     }, {
-        image: 'img/05.webp',
+        image: '05.webp',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+//select DOM element
+const slideEl = document.querySelector(".slide");
+const prevEl = document.querySelector(".prev");
+const nextEl = document.querySelector(".next");
+
+//inserisco le immagini dinamicamente
+const imgUrl = images.map((element) => {
+    return element.image
+})
+
+//console.log(imgUrl);
+//console.log(imgUrl[0]);
+let imgActive = 0;
+for (let i = 0; i < imgUrl.length; i++) {
+    const element = imgUrl[i];
+    const imgMarkup =  `<img class="${i === imgActive ? "active" : ""}" src="./assets/img/${element}" alt="">`;
+    slideEl.insertAdjacentHTML("afterbegin",imgMarkup) 
+    
+}
+
+// let imgActive = 0;
+// imgUrl.forEach((element,i=0) => {
+//     element = imgUrl[i];
+//     imgMarkup = `<img class="${i === imgActive ? "active" : ""}" src="./assets/img/${element}" alt="">`;
+//     slideEl.insertAdjacentHTML("afterbegin",imgMarkup); 
+// })
+
+
+console.log(imgUrl);
+//aggiungo un event listener ai miei bottoni
+
+
+
+nextEl.addEventListener("click", function () {
+   const slides = document.querySelectorAll(".slide > img")
+   console.log(slides);
+
+    const currentImg = slides[imgActive];
+    console.log(currentImg);
+    currentImg.classList.remove("active");
+    imgActive++
+
+    const nextImg = slides[imgActive];
+    nextImg.classList.add("active");
+    
+   
+})
